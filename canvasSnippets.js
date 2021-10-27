26e41b324ae16a8ed83f4bc099bf5f965883a80b
Aaron Shannon-Honson <aaron.shannon-honson@adelaide.edu.au>
2021-10-27 14:53:43 +1030

    Add to re-usable Canvas snippets to place into themes

--- canvasSnippets.js
+++ canvasSnippets.js
@@ -1,4 +1,3 @@
-
 // ==================================
 // Snippet for Canvas theme JS to inject an iFrame that can pass through the student, course and week data.
 // ==================================
@@ -22,10 +21,60 @@ console.log(BASE_URL + param);
 
 $('<iframe>', {
     src: BASE_URL + param,
-    id:  'injectedFrame',
+    id: 'injectedFrame',
     frameborder: 0,
     scrolling: 'no',
     width: '100%',
     height: '480',
     margin: 'auto'
-    }).appendTo('#injectionSite');
+}).appendTo('#injectionSite');
+
+
+
+// ==================================
+// Snippet for Canvas theme JS to look for a 'glossary' page and highlight words found on a page that match the glossary content
+// ==================================
+
+//find glossary page
+
+// new jQuery selector for 'insensitive contains' -- contains a term and doesn't care about upper/lower case
+jQuery.expr[':'].icontains = function (a, i, m) {
+    return jQuery(a).text().toUpperCase()
+        .indexOf(m[3].toUpperCase()) >= 0;
+};
+
+var courseID = this.ENV.COURSE_ID;
+var terms = [];
+var defs = [];
+
+$.get("/api/v1/courses/" + courseID + "/pages?sort=title&order=asc", function (e) {
+    var val = e.find(x => x.title === 'Glossary');
+    console.log(val);
+    $.get("/api/v1/courses/" + courseID + "/pages/" + val.url + "", function (urlGet) {
+        var parser = new DOMParser();
+        var htmlDoc = parser.parseFromString(urlGet.body, 'text/html');
+        var defList = htmlDoc.querySelector('dl');
+        // find each word/definition pair
+        for (let i = 0; i < defList.childNodes.length; i++) {
+            if (defList.childNodes[i].nodeType != 3) {
+                if (defList.childNodes[i].tagName == 'DT') {
+                    terms[terms.length] = defList.childNodes[i].innerText;
+
+                }
+                if (defList.childNodes[i].tagName == 'DD') {
+                    defs[defs.length] = defList.childNodes[i].innerText;
+
+                }
+            }
+        }
+
+        // try to find the first instance of each word in `glossary` and wrap the term in HTML
+        console.log("Testing finding biz");
+        for (let i = 0; i < terms.length; i++) {
+            var termFound = $(".show-content:icontains('" + "" + terms[i] + "" + "')");
+            var newText = termFound.html().replace(new RegExp('(' + terms[i] + ')', 'ig'),
+                '<span class="tooltip">$1<span class="tooltiptext">' + defs[i] + '</span></span>');
+            termFound.html(newText);
+        }
+    });
+});
