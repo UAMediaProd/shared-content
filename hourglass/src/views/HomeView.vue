d7796e3d01c5491484af4937814b11688e434a8c
Aaron Honson <honsyb@gmail.com>
2022-08-11 14:39:04 +0930

    Responsiveness tweak for hourglass

--- hourglass/src/views/HomeView.vue
+++ hourglass/src/views/HomeView.vue
@@ -8,7 +8,7 @@
           xmlns="http://www.w3.org/2000/svg"
           xmlns:xlink="http://www.w3.org/1999/xlink"
           viewBox="0 0 452.3 528.25"
-          width="600px"
+          style="max-width:600px;"
         >
           <defs>
             <clipPath id="clippath">
